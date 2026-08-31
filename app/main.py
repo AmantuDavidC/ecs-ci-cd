d3e9c6b9df90590031dbaa7191ab2bffc1792856
AmantuDavidC <106496336+AmantuDavidC@users.noreply.github.com>
2026-08-31 13:00:59 +0100

    second test 2: add new response

--- app/main.py
+++ app/main.py
@@ -2,10 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# @app.get("/")
-# def index():
-#  return {"message": "Hello, World!"}
-
 @app.get("/")
 def index():
  return {"message": "This is the second app!"}
